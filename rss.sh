cdf4a0cc2ea43663958288217fef9abe030a6e6a
swindlesmccoop <swindlesmccoop@waifu.club>
2022-02-23 15:55:05 +0000

    RSS Header Creation

--- rss.sh
+++ rss.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
+USERNAME=swindlesmccoop
+GITSITE="https://git.cbps.xyz"
+FULLURL="$GITSITE"/"$USERNAME"
 
+update_rss () {
+	echo "Last thing to be done"
+}
 
 if [ -f rss.xml ]; then
 	while true; do
 		sleep 60
-		USERNAME=swindlesmccoop
-		curl -o "$USERNAME".html "https://git.cbps.xyz/$USERNAME?tab=activity"
+		curl -sLo "$USERNAME".html "$GITSITE/$USERNAME?tab=activity"
+		update_rss
 	done
 	else
-		read -p "Blog name: " BLOGNAME
-		read -p "Description of blog: " BLOGDESC
-		read -p "Blog language (if you don't know, enter 'en-us'): " BLOGLANG
-		printf '<?xml version="1.0" encoding="utf-8"?>\n<?xml-stylesheet type="text/css" href="rss.css" ?>\n<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">\n\n<channel>\n<title>"$BLOGNAME"'
+		#create rss feed skeleton
+		BLOGNAME="$USERNAME at $(echo $GITSITE | sed "s|https://||g")"
+		BLOGDESC="$(curl -sL $FULLURL?tab=activity | grep -o -P '(?<=<meta property="og:description" content=").*(?=">)')"
+		printf '<?xml version="1.0" encoding="utf-8"?>\n<?xml-stylesheet type="text/css" href="rss.css" ?>\n<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">\n\n<channel>\n<title>' > rss.xml
+		printf "$BLOGNAME" >> rss.xml
+		printf '</title>\n<description>' >> rss.xml
+		printf "$BLOGDESC" >> rss.xml
+		printf '</description>\n<language>en-us</language>\n<link>' >> rss.xml
+		printf "$FULLURL" >> rss.xml
+		printf '</link>\n\n<!-- Activity -->\n\n' >> rss.xml
 fi
